43ae7e6fbc5a76579215e2659d637ae632e604fb
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2020-10-31 21:32:14 -0400

    removed irrelevant test to lesson.

--- cypress-example/cypress/integration/page.js
+++ cypress-example/cypress/integration/page.js
@@ -4,13 +4,6 @@ describe("page render", () => {
     cy.injectAxe();
   });
 
-  it("focuses on the wrapper when you click on the button toggle", () => {
-    cy.get("#color-scheme")
-      .click()
-      .focused()
-      .should("have.id", "radio-wrapper");
-  });
-
   it("should have proper contrast", () => {
     cy.checkA11y("body", {
       runOnly: ["cat.color"],
